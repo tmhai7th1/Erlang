43fcebfcd6cf9a0d4431ff8012988ed63ab034e1
tmhai7th1 <tmhai7th1@gmail.com>
2020-05-04 07:29:33 +0700

    pp

--- reverse_kep_structure.erl
+++ reverse_kep_structure.erl
@@ -1,72 +1,59 @@
 -module(reverse_kep_structure).
 -export([start/0]).
 
-
-
-reverse_list([Head|Rest]) ->
+reverse_list([Head|Rest],Index,Lenght,Result) when is_list(Head) ->
     io:fwrite("-------------------------------~n"),
-    %io:fwrite("Index: ~tp ~n",[Index]),
-    io:fwrite("Head: ~tp ~n",[Head]),
-    io:fwrite("Rest: ~tp ~n",[Rest]),
-    Ischeck = is_list(Head) andalso lenght_List(0,Head) > 0,
-    A = 0,
+    io:fwrite("is_list Index: ~tp ~n",[Index]),
+    io:fwrite("is_list Lenght: ~tp ~n",[Lenght]),
+    io:fwrite("is_list Result: ~tp ~n",[Result]),
+    
     if
-        Ischeck ->
-            io:fwrite("ResIscheckt: ~tp ~n",[Head]),
-            List_T = convert_list_to_c(reverse_list(Head),[]),
-            [List_T|reverse_list(Rest)];
-        true ->
-            List_T = convert_list_to_c(Rest,[]),
-            [List_T|reverse_list(Rest)]
+        Index == Lenght ->
+            io:fwrite("when is_list(Head) Head == Lenght Head: ~tp ~n",[Head]),
+            Value = reverse_list(Head,Index,Lenght,Result),
+            io:fwrite("when is_list(Head) Head == Lenght Head Value: ~tp ~n",[Value]),
+            io:fwrite("when is_list(Head) Head == Lenght Head Index: ~tp ~n",[Index]),
+            io:fwrite("when is_list(Head) Head == Lenght Head Lenght: ~tp ~n",[Lenght]),
+            reverse_list([],Index,Lenght,Value);
+        true->
+            io:fwrite("Value Index != Lenght: ~tp ~n",[Result]),
+            Value = reverse_list(Head,Index,Lenght,Result),
+            reverse_list(Rest,Value,Lenght,Result)
     end;
-    %io:fwrite("reverse_list Head: ~tp ~n",[Head]),
-    %Value = find_item_byindex(0,Lenght - Index,Result_List),
-   % io:fwrite("reverse_list Value: ~tp ~n",[Value]),
    
+reverse_list([],Index,Lenght,Result) when Index == Lenght->
+        io:fwrite("Result Index == Lenght Result: ~tp ~n",[Result]),
+        Result;
 
-reverse_list([]) ->
-    %io:fwrite("final Index: ~tp ~n",[Index]),
-    %io:fwrite("------------------------------- final Head ~tp ~n",[Result_List]),
-    [].
+reverse_list([Head|Rest],Index,Lenght,Result) ->
+    io:fwrite("-------------------------------~n"),
+    io:fwrite("Index: ~tp ~n",[Index]),
+    io:fwrite("Lenght: ~tp ~n",[Lenght]),
+    io:fwrite("Head: ~tp ~n",[Head]),
+    if
+        Index == Lenght ->
+            io:fwrite("Value Index == Lenght: ~tp ~n",[Head]),
+            reverse_list([],Index,Lenght,Head);
+        true->
+            io:fwrite("Value Index != Lenght: ~tp ~n",[Result]),
+            reverse_list(Rest,Index+1,Lenght,Result)
+    end;
 
-find_item_byindex(Index,Lenght,[Head|Rest]) when is_list(Head) ->
-    io:fwrite("------------------------------- when is_list(Head) ~n"),
-    io:fwrite("is_list(Head) Index find_item_byindex: ~tp ~n",[Index]),
-    find_item_byindex(Index,Lenght,Head);
-   % io:fwrite("is_list(Head) Head find_item_byindex: ~tp ~n",[List]),
-    %find_item_byindex (Index,Lenght,Rest);
 
- find_item_byindex(Int,Index,[Head|_]) when Index == Int ->
-    io:fwrite("------------------------------- final Head ~tp ~n",[Head]),
-        Head;
-
-find_item_byindex(Int,Index,[_|Rest]) ->
-    io:fwrite("------------------------------- ~n"),
-    io:fwrite("find_item_byindex Int: ~tp ~n",[Int]),
-    io:fwrite("find_item_byindex Value: ~tp ~n",[Index]),
-    find_item_byindex(Int + 1,Index,Rest).
-
-lenght_List(Index,[Head|Rest])->
-    lenght_List(Index+1,Rest);
-
-lenght_List(Index,[])->
+reverse_list([],Index,Lenght,Result)->
+    io:fwrite("-------------------------------~n"),
+    io:fwrite("Lenght: ~tp ~n",[Lenght]),
+    io:fwrite("Index: ~tp ~n",[Index]),
+    io:fwrite("Result: ~tp ~n",[Result]),
     Index.
 
-
-convert_list_to_c([City | Rest],Result_List) ->
-    io:fwrite("-------------------------------~tp ~n",[City]),
- convert_list_to_c(Rest,[City|Result_List]);
-convert_list_to_c([],Result_List) ->
-    Result_List.
-
-
-
 start()->
-    List = [1,2,[3,4],6,7],
+    List = [1,2,[3,4,[5,6,7]],8,9],
     List1 = [1,2,3],
-    Lenght = 4,
+    Lenght =4,
     Index = 0,
+    Result = 0,
     %lenght_List(Index,List1).
     %find_item_byindex(0,Lenght,List).
-  reverse_list(List).
+  reverse_list(List,Index,Lenght,Result).
 %convert_list_to_c(List1,[]).
